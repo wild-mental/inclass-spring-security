ae8bdaf1d1184cfa4984fe6cc77b26df261c0323
wild-mental <wild.mental@kidsnote.com>
2024-05-16 17:06:12 +0900

    initial commit

---
+++ src/main/java/ac/su/inclassspringsecurity/InclassSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package ac.su.inclassspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclassSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InclassSpringSecurityApplication.class, args);
+    }
+
+}
