b884f9ffe59a7aef94643517054624c966882bd7
wild-mental <wild.mental@kidsnote.com>
2024-05-16 17:06:43 +0900

    1. 상품 데이터 클래스 정의

---
+++ src/main/java/ac/su/inclassspringsecurity/constant/ProductStatusEnum.java
@@ -0,0 +1,8 @@
+package ac.su.inclassspringsecurity.constant;
+
+public enum ProductStatusEnum {
+    // 1) 판매 상태
+    PREPARING, IN_STOCK, SOLD_OUT,
+    // 2) 데이터 상태
+    INVALID, DELETED
+}
---
+++ src/main/java/ac/su/inclassspringsecurity/domain/Product.java
@@ -0,0 +1,57 @@
+package ac.su.inclassspringsecurity.domain;
+
+import ac.su.inclassspringsecurity.constant.ProductStatusEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int price;
+    @Column(nullable = false)
+    private int stockCount;
+    @Column(nullable = false)
+    private ProductStatusEnum status;
+    // 등록 및 수정 시각
+    @Column(nullable = false)
+    private String createdAt;
+    @Column(nullable = false)
+    private String updatedAt;
+
+    // 추가 데이터 컬럼
+    // 상품 상세 정보
+    @Column(nullable = false)
+    private String description;
+
+    // 상품 메모
+    @Column(nullable = false)
+    private String memo;
+
+    // 상품 이미지
+    @Column(nullable = false)
+    private String image;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", price=" + price +
+            ", stockCount=" + stockCount +
+            ", status=" + status +
+            ", createdAt='" + createdAt + '\'' +
+            ", updatedAt='" + updatedAt + '\'' +
+            ", description='" + description + '\'' +
+            ", memo='" + memo + '\'' +
+            ", image='" + image + '\'' +
+            '}';
+    }
+}
