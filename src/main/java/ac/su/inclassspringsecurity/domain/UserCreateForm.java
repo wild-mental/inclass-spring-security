1a81fbccf42903ae4ad3badc71e7987044f811ae
wild-mental <wild.mental@kidsnote.com>
2024-06-21 10:15:15 +0900

    UserCreateForm 예제 주석 추가 및 Repository 쿼리 수정

--- src/main/java/ac/su/inclassspringsecurity/domain/UserCreateForm.java
+++ src/main/java/ac/su/inclassspringsecurity/domain/UserCreateForm.java
@@ -1,11 +1,8 @@
 package ac.su.inclassspringsecurity.domain;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 
 @Getter @Setter // Setter 를 제거하고 immutable 하게 다루기 위한 조치 필요
 public class UserCreateForm {  // DTO 클래스 역할을 하는 Form 클래스
@@ -22,4 +19,8 @@ public class UserCreateForm {  // DTO 클래스 역할을 하는 Form 클래스
     @NotEmpty(message = "이메일은 필수항목입니다.")
     @Email
     private String email;
+
+    // 가입 일자 검증 예제
+//    @FutureOrPresent
+//    private String createdAt;
 }
--- src/main/java/ac/su/inclassspringsecurity/repository/UserRepository.java
+++ src/main/java/ac/su/inclassspringsecurity/repository/UserRepository.java
@@ -11,8 +11,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByUsername(String username);
-//    Optional<User> findByUsername(String username);
+    // List<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     // (1) 중복 유저 체크를 위한 기본 쿼리 (username, email) 준비
     boolean existsByUsername(String username);
